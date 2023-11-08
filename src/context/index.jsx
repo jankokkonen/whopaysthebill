7785f8bf11d410ba4279d1aa74dbd5bd27669224
jankokkonen <jan.kokkonen7@gmail.com>
2023-11-08 16:22:59 +0200

    Create some useState funtionality

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -3,5 +3,21 @@ import { useState, createContext } from "react";
 const MyContext = createContext();
 
 const MyProvider = (props) => {
-  return <MyContext.Provider>{props.children}</MyContext.Provider>;
+  const [stage, setStage] = useState(1);
+  const [players, setPlayers] = useState([]);
+  const [result, setResult] = useState("");
+
+  return (
+    <MyContext.Provider
+      value={{
+        stage: stage,
+        players: players,
+        result: result,
+      }}
+    >
+      {props.children}
+    </MyContext.Provider>
+  );
 };
+
+export { MyContext, MyProvider };

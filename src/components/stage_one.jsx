8e434e3b9bf9d5a1526d86f8973cc346b9f8bb31
jankokkonen <jan.kokkonen7@gmail.com>
2023-11-08 15:49:36 +0200

    Add necessary files and import bootstrap css

---
+++ src/components/stage_one.jsx
@@ -0,0 +1,5 @@
+const stage_one = () => {
+  return <div>stage_one</div>;
+};
+
+export default stage_one;
---
+++ src/components/stage_two.jsx
@@ -0,0 +1,5 @@
+const stage_two = () => {
+  return <div>stage_two</div>;
+};
+
+export default stage_two;

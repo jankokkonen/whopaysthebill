94ee53af9fb1e70e4e9ea5de2dd0f707bf9b0f34
jankokkonen <jan.kokkonen7@gmail.com>
2023-11-09 15:31:03 +0200

    Create stage1 component form functionality

--- src/components/stage_one.jsx
+++ src/components/stage_one.jsx
@@ -1,5 +1,36 @@
+import { useContext, useRef } from "react";
+import { MyContext } from "../context";
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
+
 const Stage1 = () => {
-  return <div>Stage1</div>;
+  const textInput = useRef();
+  const context = useContext(MyContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = textInput.current.value;
+    textInput.current.value = "";
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="mt-4">
+        <Form.Group>
+          <Form.Control
+            type="text"
+            placeholder="Add player name"
+            name="player"
+            ref={textInput}
+          />
+        </Form.Group>
+        <Button className="miami" variant="primary" type="submit">
+          Add player
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default Stage1;

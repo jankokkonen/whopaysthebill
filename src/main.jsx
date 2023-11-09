51bfdb49c2d614420c6db0d49510b91ad178c6d0
jankokkonen <jan.kokkonen7@gmail.com>
2023-11-09 14:26:41 +0200

    Import MyProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import { MyProvider } from "./context/index.jsx";
+import { MyProvider } from "./context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <MyProvider>
     <App />
-  </React.StrictMode>
+  </MyProvider>
 );
